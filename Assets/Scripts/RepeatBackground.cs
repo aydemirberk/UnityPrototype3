6bb3cbd81a800f039a0604a578d04b42ca7d56ed
aydemirberk <beerkaydemir@gmail.com>
2022-05-25 22:45:33 +0300

    Prototype 3 Final Version

---
+++ Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatWitdh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        repeatWitdh = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < startPos.x-repeatWitdh)
+        {
+            transform.position = startPos;
+        }
+    }
+}
